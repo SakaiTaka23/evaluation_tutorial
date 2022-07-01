65e6a1fd5ed842d54420588acb4f928ad495ffd5
SakaiTaka23 <tt3.sakai@gmail.com>
2022-07-01 20:44:37 +0900

    counterのみ動作検証

--- try-prometheus/nodejs-client/app/src/app.controller.ts
+++ try-prometheus/nodejs-client/app/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { InjectMetric } from '@willsoto/nestjs-prometheus';
+import { Counter } from 'prom-client';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectMetric('app_access') public counter: Counter<string>,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.counter.inc();
     return this.appService.getHello();
   }
 }
--- try-prometheus/nodejs-client/app/src/app.module.ts
+++ try-prometheus/nodejs-client/app/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import {
+  makeCounterProvider,
+  PrometheusModule,
+} from '@willsoto/nestjs-prometheus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PrometheusModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    makeCounterProvider({
+      name: 'app_access',
+      help: 'access time of app controller',
+    }),
+  ],
 })
 export class AppModule {}
