9d3a3161728b2b71bb581679e2e16237274950b9
SakaiTaka23 <tt3.sakai@gmail.com>
2023-04-23 13:00:06 +0900

    jaegerでトレースが表示されていることを確認

--- try-opentelemtry/nest/src/tracing.ts
+++ try-opentelemtry/nest/src/tracing.ts
@@ -1,7 +1,4 @@
-import {
-  ConsoleSpanExporter,
-  SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
@@ -9,14 +6,17 @@ import * as process from 'process';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { Resource } from '@opentelemetry/resources';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 
-const traceExporter = new ConsoleSpanExporter();
+const jaegerExporter = new JaegerExporter({
+  endpoint: 'http://localhost:14268/api/traces',
+});
 
 export const otelSDK = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'nestjs-otel',
   }),
-  spanProcessor: new SimpleSpanProcessor(traceExporter),
+  spanProcessor: new SimpleSpanProcessor(jaegerExporter),
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
