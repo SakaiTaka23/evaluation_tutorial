80db1a2a3e64134ef686fb58351861788f94a74b
SakaiTaka23 <tt3.sakai@gmail.com>
2023-04-23 13:10:37 +0900

    環境に応じてトレースの処理を帰れるようにした

--- try-opentelemtry/nest/src/tracing.ts
+++ try-opentelemtry/nest/src/tracing.ts
@@ -1,22 +1,32 @@
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import {
+  BatchSpanProcessor,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import * as process from 'process';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { Resource } from '@opentelemetry/resources';
 import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+import * as process from 'process';
 
 const jaegerExporter = new JaegerExporter({
   endpoint: 'http://localhost:14268/api/traces',
 });
 
+const spanProcessor =
+  process.env.NODE_ENV === `development`
+    ? new SimpleSpanProcessor(jaegerExporter)
+    : new BatchSpanProcessor(jaegerExporter);
+
+console.log(process.env.NODE_ENV);
+
 export const otelSDK = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'nestjs-otel',
   }),
-  spanProcessor: new SimpleSpanProcessor(jaegerExporter),
+  spanProcessor,
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
